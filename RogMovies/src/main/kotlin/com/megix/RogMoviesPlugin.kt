833af13762cf16bf91b10ad77a53adf071fa2888
MrXtron <srb0123-git@yahoo.com>
2026-04-16 19:38:18 +0530

    Add files via upload

---
+++ RogMovies/src/main/kotlin/com/megix/RogMoviesPlugin.kt
@@ -0,0 +1,14 @@
+package com.megix
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class RogMoviesPlugin: Plugin() { // All providers should be added in this manner. Please don't edit the providers list directly.
+    override fun load(context: Context) {
+        registerMainAPI(RogmoviesProvider())
+        registerExtractorAPI(VCloud())
+    }
+}
+
