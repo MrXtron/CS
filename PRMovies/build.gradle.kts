449013b9939bf7b3272e2df625449df2c84a1e4c
MrXtron <srb0123-git@yahoo.com>
2026-04-19 19:27:20 +0530

    Add files via upload

--- PRMovies/build.gradle.kts
+++ PRMovies/build.gradle.kts
@@ -1,1 +1,25 @@
-gg
+version = 1
+
+
+cloudstream {
+    language = "hi"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "PR Movies"
+     authors = listOf(" ")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/MrXtron/CSF/refs/heads/main/Icons/PrMovies.png"
+}
