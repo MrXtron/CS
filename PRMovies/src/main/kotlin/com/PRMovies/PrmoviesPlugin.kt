fcc3ba1a10dc4aacf260213b60c346f33ce85357
MrXtron <srb0123-git@yahoo.com>
2026-04-20 11:09:57 +0530

    Update PrmoviesPlugin.kt

--- PRMovies/src/main/kotlin/com/PRMovies/PrmoviesPlugin.kt
+++ PRMovies/src/main/kotlin/com/PRMovies/PrmoviesPlugin.kt
@@ -1,13 +1,13 @@
-package com.PRMovies
-
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
-
-@CloudstreamPlugin
-class PrmoviesPlugin: Plugin() {
-    override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(PrmoviesProvider())
+companion object {
+    suspend fun getDomains(): PrmoviesDomains? {
+        return try {
+            app.get("https://raw.githubusercontent.com/MrXtron/CSF/refs/heads/main/domains.json").parsed<PrmoviesDomains>()
+        } catch (e: Exception) {
+            null
+        }
     }
 }
+
+data class PrmoviesDomains(
+    @JsonProperty("PRMovies") val PRMovies: String? = null
+)
