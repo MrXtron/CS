e60e506e2a53b3504b9805ffe612179efd05497e
MrXtron <srb0123-git@yahoo.com>
2026-04-20 11:26:06 +0530

    Update PrmoviesPlugin.kt

--- PRMovies/src/main/kotlin/com/PRMovies/PrmoviesPlugin.kt
+++ PRMovies/src/main/kotlin/com/PRMovies/PrmoviesPlugin.kt
@@ -1,13 +1,28 @@
-companion object {
-    suspend fun getDomains(): PrmoviesDomains? {
-        return try {
-            app.get("https://raw.githubusercontent.com/MrXtron/CSF/refs/heads/main/domains.json").parsed<PrmoviesDomains>()
-        } catch (e: Exception) {
-            null
+package com.PRMovies
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.app
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@CloudstreamPlugin
+class PrmoviesPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(PrmoviesProvider())
+    }
+
+    companion object {
+        suspend fun getDomains(): PrmoviesDomains? {
+            return try {
+                app.get("https://raw.githubusercontent.com/MrXtron/CSF/refs/heads/main/domains.json").parsed<PrmoviesDomains>()
+            } catch (e: Exception) {
+                null
+            }
         }
     }
-}
 
-data class PrmoviesDomains(
-    @JsonProperty("PRMovies") val PRMovies: String? = null
-)
+    data class PrmoviesDomains(
+        @JsonProperty("PRMovies") val PRMovies: String? = null
+    )
+}
