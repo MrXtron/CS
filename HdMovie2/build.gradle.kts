e7e1ce1cb08e9fd30539ecac278d4c1625952dea
MrXtron <srb0123-git@yahoo.com>
2026-04-21 13:55:47 +0530

    Update build.gradle.kts

--- HdMovie2/build.gradle.kts
+++ HdMovie2/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     language = "hi"
     // All of these properties are optional, you can safely remove them
 
-     description = "Includes: Hdmovie2"
+     description = "Hdmovie2"
      authors = listOf("-")
 
     /**
